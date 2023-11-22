6d86689ef6372e4cafdd8a446efd04a8f4d76168
debater-coder <52619668+debater-coder@users.noreply.github.com>
2023-11-22 18:31:42 +1100

    no uefi_services

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,20 @@
 #![no_main]
 #![no_std]
 
+use core::fmt::Write;
+use core::panic::PanicInfo;
 use uefi::prelude::*;
 
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
 #[entry]
-fn main(_image_handle: Handle, mut system_table: SystemTable<Boot>) -> ! {
+fn main(_image_handle: Handle, mut system_table: SystemTable<Boot>) -> Status {
     let stdout = system_table.stdout();
-    stdout.clear().unwrap().unwrap();
-    writeln!(stdout, "Hello World!").unwrap();
+    stdout.clear().unwrap();
+    stdout.write_str("Hello, World!").unwrap();
 
     loop {}
 }
